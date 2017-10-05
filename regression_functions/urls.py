196c7137127c2462e5a2a9511d59e5cf0ffc5944
Darshit <darshit7@gmail.com>
2017-10-05 22:36:06 +0530

    Import required views.

--- regression_functions/urls.py
+++ regression_functions/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from regression.views import IndexView, LinerReg
+from regression.views import IndexView, LinerReg, ArrheniusView
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name="index"),
